89c204824939f71e3843a0a6787683789b0fd350
Yogesh <yogeshthakur938920@gmail.com>
2025-09-23 20:34:31 +0530

    saved changes

--- client/app/api/products/route.js
+++ client/app/api/products/route.js
@@ -1,5 +1,26 @@
+import prisma from "@/lib/prisma";
 
 
 export async function GET(request) {
+  try {
     
+    let products = await prisma.product.findMany({
+        where: {inStock:true},
+        include:{
+            rating:{
+                select:{
+                    createdAt:true , rating:true, review:true , 
+                    user:{ select:{name:true , image:true}}
+                }
+            }
+        }
+    })
+
+  } catch (error) {
+    console.error("GET error:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }
